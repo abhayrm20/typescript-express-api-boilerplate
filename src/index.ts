6aefac45b840a157a932122bf262eba3d49d876a
Abhay <abhayrm20>
2021-07-08 03:11:28 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,45 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import moment from 'moment-timezone';
-import fs from 'fs';
 
 const start = async () => {
-  try {
-    [
-      'JWT_SECRET',
-      'ENVIRONMENT',
-    ].forEach((envVar) => {
-      if (!process.env[envVar]) {
-        throw new Error(`${envVar} not defined.`);
-      }
-    });
+    try {
+        [
+            'JWT_SECRET',
+            'ENVIRONMENT',
+        ].forEach((envVar) => {
+            if (!process.env[envVar]) {
+                throw new Error(`${envVar} not defined.`);
+            }
+        });
 
-    if (process.env.ENVIRONMENT! === 'prod') {
-      await mongoose.connect(`${process.env.MONGO_URI_PROD!}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      });
-    } else {
-      await mongoose.connect(process.env.MONGO_URI_DEV!, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      });
+        if (process.env.ENVIRONMENT! === 'prod') {
+            await mongoose.connect(`${process.env.MONGO_URI_PROD!}`, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+            });
+        } else {
+            await mongoose.connect(process.env.MONGO_URI_DEV!, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+            });
+        }
+        console.log('Connected to MongoDB');
+
+        moment.tz.setDefault('Asia/Kolkata');
+
+        const port = process.env.PORT || 3000;
+
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}!!`);
+        });
+    } catch (err) {
+        console.error(err);
     }
-    console.log('Connected to MongoDB');
-
-    moment.tz.setDefault('Asia/Kolkata');
-
-    app.listen(3000, () => {
-        console.log('Listening on port 3000!!');
-    });
-  } catch (err) {
-    console.error(err);
-  }
 };
 
 start();
